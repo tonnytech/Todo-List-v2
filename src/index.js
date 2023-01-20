ab7b519edc21385ebe85182cfcb054b1004e429b
teitonny <tonnytei4@gmail.com>
2023-01-20 19:57:54 +0300

    removed hard codes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './index.css';
 
 const myListItems = document.querySelector('.to-do-list');
+const toDoItem = document.getElementById('toDoItem');
 
 const ToDoListArray = [
   {
@@ -10,19 +11,49 @@ const ToDoListArray = [
   },
   {
     description: 'take breakfast',
-    index: 1,
+    index: 2,
     completed: false,
   },
   {
     description: 'play football',
-    index: 1,
+    index: 3,
     completed: false,
   },
 ];
 
+const getBookStorage = () => {
+  let taskListStorage;
+  if (JSON.parse(localStorage.getItem('taskList')) === null) {
+    taskListStorage = [];
+  } else {
+    taskListStorage = JSON.parse(localStorage.getItem('taskList'));
+  }
+  return taskListStorage;
+};
+
+const addTask = (task, complete = false, index) => {
+  const toDoTask = task;
+  const toDoComplete = complete;
+  const toDoIndex = index;
+
+  localStorage.setItem(
+    'taskList',
+    JSON.stringify({
+      completed: toDoComplete,
+      index: toDoIndex,
+      description: toDoTask,
+    }),
+  );
+};
+
+const addEvent = () => {
+  addTask(toDoItem.value, false, )
+}
+
 const AddToDoListItems = (listItem) => {
   const mainElement = document.createElement('li');
   mainElement.classList.add('to-do-pop');
+  mainElement.dataset.id = listItem.index;
 
   const doneCheckbox = document.createElement('input');
   doneCheckbox.setAttribute('type', 'checkbox');
@@ -36,6 +67,17 @@ const AddToDoListItems = (listItem) => {
   toDoInput.setAttribute('value', listItem.description);
   toDoInput.classList.add('to-do-input');
   mainElement.appendChild(toDoInput);
+
+  toDoInput.addEventListener('click', () => {
+    toDoInput.removeAttribute('readonly');
+  });
+
+  toDoInput.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      const updatedTask = toDoInput.value;
+    }
+  });
 
   const deleteBtn = document.createElement('i');
   deleteBtn.classList.add('fa-solid');
