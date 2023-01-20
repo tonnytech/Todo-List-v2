aa888c40acd50935b08fde625e7f430ddc8a14a5
teitonny <tonnytei4@gmail.com>
2023-01-20 10:47:07 +0300

    removed linter error

--- src/index.js
+++ src/index.js
@@ -1,45 +1,45 @@
-import "./index.css";
+import './index.css';
 
-const myListItems = document.querySelector(".to-do-list");
+const myListItems = document.querySelector('.to-do-list');
 
 const ToDoListArray = [
   {
-    description: "wash dishes",
+    description: 'wash dishes',
     index: 1,
     completed: false,
   },
   {
-    description: "take breakfast",
+    description: 'take breakfast',
     index: 1,
     completed: false,
   },
   {
-    description: "play football",
+    description: 'play football',
     index: 1,
     completed: false,
   },
 ];
 
 const AddToDoListItems = (listItem) => {
-  const mainElement = document.createElement("li");
-  mainElement.classList.add("to-do-pop");
+  const mainElement = document.createElement('li');
+  mainElement.classList.add('to-do-pop');
 
-  const doneCheckbox = document.createElement("input");
-  doneCheckbox.setAttribute("type", "checkbox");
+  const doneCheckbox = document.createElement('input');
+  doneCheckbox.setAttribute('type', 'checkbox');
   doneCheckbox.checked = listItem.complete;
-  doneCheckbox.classList.add("my-checkbox");
+  doneCheckbox.classList.add('my-checkbox');
   mainElement.appendChild(doneCheckbox);
 
-  const toDoInput = document.createElement("INPUT");
-  toDoInput.setAttribute("type", "input");
-  toDoInput.setAttribute("readonly", true);
-  toDoInput.setAttribute("value", listItem.description);
-  toDoInput.classList.add("to-do-input");
+  const toDoInput = document.createElement('INPUT');
+  toDoInput.setAttribute('type', 'input');
+  toDoInput.setAttribute('readonly', true);
+  toDoInput.setAttribute('value', listItem.description);
+  toDoInput.classList.add('to-do-input');
   mainElement.appendChild(toDoInput);
 
-  const deleteBtn = document.createElement("i");
-  deleteBtn.classList.add("fa-solid");
-  deleteBtn.classList.add("fa-ellipsis-vertical");
+  const deleteBtn = document.createElement('i');
+  deleteBtn.classList.add('fa-solid');
+  deleteBtn.classList.add('fa-ellipsis-vertical');
   // deleteBtn.textContent = "remove";
   mainElement.appendChild(deleteBtn);
 
