3c3cee4e38b41c2171abec08b9f954cd1c53848d
teitonny <tonnytei4@gmail.com>
2023-01-23 20:48:19 +0300

    added add and remove class

--- src/modules/getStorage.js
+++ src/modules/getStorage.js
@@ -1,13 +1,1 @@
 // Get data from local storage
-
-const getBookStorage = () => {
-  let taskListStorage;
-  if (JSON.parse(localStorage.getItem('taskList')) === null) {
-    taskListStorage = [];
-  } else {
-    taskListStorage = JSON.parse(localStorage.getItem('taskList'));
-  }
-  return taskListStorage;
-};
-
-export default getBookStorage;
